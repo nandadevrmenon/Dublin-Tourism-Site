b486cbd2ae8713a36871ff9ce717e3f12997fea4
ipuig <ivanbruno.puigdantas@student.griffith.ie>
2022-05-08 15:23:46 +0100

    Implement observer

--- js/index.js
+++ js/index.js
@@ -1,8 +1,16 @@
+'use strict'
+const decoration = document.querySelector('#home_page > main > section:first-child > div:first-child');
+const decorationElement = document.querySelector('#home_page > main > section:first-child > div:last-child');
+
 const observer = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
-        console.log(entry)
-    }, {})
+        if(entry.isIntersecting)
+        entry.target.classList.toggle('inverse');
+    }, {
+        root: null,
+        threshold: 1,
 
+    })
 });
 
-console.log(observer);
+observer.observe(decorationElement);
