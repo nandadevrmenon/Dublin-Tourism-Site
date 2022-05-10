621132e95b9815b4716bf18afc63a51c02da8817
ipuig <ivanbruno.puigdantas@student.griffith.ie>
2022-05-10 13:58:11 +0100

    Implement main info scrolling effect

--- js/observable.js
+++ js/observable.js
@@ -1,16 +1,19 @@
 'use strict'
 
-const articles = document.querySelectorAll('home_page > main > section:last-child > article')
+const articles = document.querySelectorAll('#home_page > main > section:last-child > article')
 const observerOptions = {
-    root: null,
-    threshold: 0
+    root: null, 
+    threshold: 0,
+    rootMargin: "-300px"
 }
 
-const observer = new IntersectionObserver(entries => entries.forEach(entry => {
-    console.log(entry.target);
-    }, observerOptions));
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    entry.target.classList.toggle("slide", entry.isIntersecting);
+    })
+}, observerOptions);
 
-articles.forEach(observer.observe);
+articles.forEach(article => observer.observe(article));
 
 
 
