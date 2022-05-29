3ed9d4df74a4d53ba3d630af9eb72d58e2869858
ipuig <ivanbruno.puigdantas@student.griffith.ie>
2022-05-29 19:18:20 +0100

    Retrieve JSON

--- js/observable.js
+++ js/observable.js
@@ -3,7 +3,6 @@
 let articles = Array.from(document.querySelectorAll('#home_page > main > section:last-child > article'));
 let layout = "default";
 const smallScreen = window.matchMedia("(max-width: 850px)");
-// const tinyScreen = window.matchMedia("(max-width: 500px)");
 
 const observerOptions = {
     root: null, 
@@ -23,11 +22,6 @@ const smallScreenHandler = mediaquery => {
     if (mediaquery.matches) smallLayout();
     else defaultLayout();
 }
-
-// const tinyScreenHandler = mediaquery => {
-//     if (mediaquery.matches) tinyLayout();
-//     else defaultLayout();
-// }
 
 const defaultLayout = () => {
     if(layout === "default") return; 
@@ -77,41 +71,5 @@ const smallLayout = () => {
     layout = "small";
 }
 
-// const tinyLayout = () => {
-//     if(layout === "tiny") return;
-//     articles.forEach((article, index) => {
-
-//         if(layout === "small") {
-
-//         }
-
-//         else {
-//             if(index % 2 !== 0) {
-//                 const header = article.children[1].children[0];
-//                 const p1 = article.children[1].children[1];
-//                 const p2 = article.children[1].children[2];
-//                 const button = article.children[1].children[3];
-//                 const figure = article.children[0];
-//                 article.innerHTML = '';
-//                 article.append(header, figure, p1, p2, button);
-//             }
-//             else {
-//                 const header = article.children[0].children[0];
-//                 const p1 = article.children[0].children[1];
-//                 const p2 = article.children[0].children[2];
-//                 const button = article.children[0].children[3];
-//                 const figure = article.children[1];
-//                 article.innerHTML = '';
-//                 article.append(header, figure, p1, p2, button);
-//             }
-//         }
-
-//     });
-
-//     layout = "tiny";
-// }
-
 smallScreenHandler(smallScreen);
-// tinyScreenHandler(tinyScreen);
 smallScreen.addEventListener('change', smallScreenHandler);
-// tinyScreen.addEventListener('change', tinyScreenHandler);
