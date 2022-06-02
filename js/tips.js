c5e3af3e34d06453dcfde8359b69375cfa485a49
ibpd <dev@bpuig.com>
2022-06-02 13:09:01 +0100

    Add travel tips skeleton and images

---
+++ js/tips.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const section = document.querySelector('#travel_tips #tips');
+
+const divImg = document.createElement('div'); 
+const h3 = document.createElement('h3');
+const p = document.createElement('p');
+const anchor = document.createElement('a');
